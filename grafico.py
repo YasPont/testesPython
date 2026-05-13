b36e37f4f65216a8233576f0eeec797ef73c58c2
YasPont <132621006+YasPont@users.noreply.github.com>
2026-05-13 13:20:05 -0300

    Atualizar o grafico.py

--- grafico.py
+++ grafico.py
@@ -8,6 +8,7 @@ T = [1.01, 1.43, 1.70, 2.02, 2.19, 2.35, 2.44]  # s
 plt.figure(figsize=(7, 5))
 plt.scatter(T, L, color='blue', label='Experimental')
 plt.plot(T, L, color='magenta', label='Potência (Experimental)')
+
 plt.title("Gráfico de T em função de L")
 plt.xlabel("T (seg)")
 plt.ylabel("L (cm)")
